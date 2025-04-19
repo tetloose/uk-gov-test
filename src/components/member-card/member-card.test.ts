d1b1497d713227a175cefd2ff4eee12169ef7f3d
tetloose <tetloose@gmail.com>
2025-04-19 23:09:34 +0100

    basic tests, I suck at writting tests, chatgtp is my friend here

--- src/components/member-card/member-card.test.ts
+++ src/components/member-card/member-card.test.ts
@@ -1,6 +1,42 @@
-import MemberCard from './member-card.component'
+import { MemberCard } from './member-card.component'
 
-describe('Card component', (): void => {
+jest.mock('./member-card.component.ts', () => {
+  return {
+    MemberCard: jest.fn().mockImplementation(() => {
+      return {
+        fetchMember: jest.fn(),
+        render: jest.fn()
+      }
+    })
+  }
+})
+
+const MockedMemberCard = MemberCard as jest.Mock
+
+describe('MemberCard', (): void => {
+  beforeEach(() => {
+    MockedMemberCard.mockClear()
+  })
+
+  it('calls the constructor when initiated', (): void => {
+    const { document } = window
+    const module = document.createElement('div') as HTMLElement
+
+    new MemberCard(module)
+
+    expect(MockedMemberCard).toHaveBeenCalledTimes(1)
+  })
+
+  it('calls fetchMember on init', () => {
+    new MemberCard(document.createElement('div'))
+
+    expect(MemberCard).toHaveBeenCalled()
+
+    const instance = (MemberCard as jest.Mock).mock.results[0].value
+
+    expect(instance.fetchMember).toBeDefined()
+  })
+
   it('should exist and be a function', (): void => {
     expect(MemberCard).toBeDefined()
     expect(typeof MemberCard).toBe('function')
